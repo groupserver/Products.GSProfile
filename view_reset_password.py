1cee1182af19f5002b9b8dd7e8d80d546e6f42df
Michael JasonSmith <mpj17@onlinegroups.net>
2008-01-15 03:42:06 +0000

    Moving the set-password redirect to "r", and adding supporing pages.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSProfile/trunk%403065

--- view_reset_password.py
+++ view_reset_password.py
@@ -1,42 +1,57 @@
 # coding=utf-8
 import Globals
 from Products.Five import BrowserView
-from zope.component import createObject
+from Products.Five.traversable import Traversable
+from zope.app.traversing.interfaces import ITraversable
+from zope.interface import implements
 
 from interfaces import *
 
 import logging
 log = logging.getLogger('GSProfile')
 
-class GSResetPasswordRedirect(BrowserView):
+class GSResetPasswordRedirect(BrowserView, Traversable):
+    implements(ITraversable)
+    
     '''View object for standard GroupServer User-Profile Instances'''
     def __init__(self, context, request):
         assert context
         assert request
         self.context = context
         self.request = request
-        siteInfo = createObject('groupserver.SiteInfo', context)
- 
-        verificationId = request.get('verificationId', '')
-        assert verificationId, 'Verification ID not set'
-        self.verificationId = verificationId
+
+        request.form['subpaths'] = []
+
+    def traverse(self, name, furtherPath):
+        self.request.form['subpaths'].append(name)
+        
+        return self
 
     def login_redirect(self):
         site_root = self.context.site_root()
         acl_users = site_root.acl_users
-        user = acl_users.get_userByPasswordVerificationId(self.verificationId)
-        assert user
 
-        m = 'GSPasswordResetRedirect: Going to the set password page '\
-          'for the user %s (%s), using the verification ID "%s"' %\
-          (user.getProperty('fn', ''), user.getId(), self.verificationId)
-        log.info(m)
-
-        site_root.cookie_authentication.credentialsChanged(user, 
-          user.getId(), user.get_password())
-
-        user.clear_userPasswordResetVerificationIds()
-
-        url = '/contacts/%s/password.html' % user.getId()
-        return self.request.RESPONSE.redirect(url)
+        if len(self.request.form['subpaths']) == 1:
+            verificationId = self.request.form['subpaths'][0]
+            user = acl_users.get_userByPasswordVerificationId(verificationId)
+            
+            if user:
+                m = 'GSPasswordResetRedirect: Going to the set password '\
+                  'page for the user %s (%s), using the verification ID '\
+                  '"%s"' % (user.getProperty('fn', ''), user.getId(),
+                    verificationId)
+                log.info(m)
+                
+                # Log in
+                site_root.cookie_authentication.credentialsChanged(user,
+                  user.getId(), user.get_password())
+                # Clean up
+                user.clear_userPasswordResetVerificationIds()
+                
+                uri = '/contacts/%s/password.html' % user.getId()
+            else: # Cannot find user
+                uri = '/r/user-not-found?id=%s' % verificationId
+        else: # Verification ID not specified
+            uri = '/r/user-no-id'
+        return self.request.RESPONSE.redirect(uri)
 
