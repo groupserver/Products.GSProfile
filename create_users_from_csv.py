3c1beda9263266f55e01ea3a5c95994df66c2041
Michael JasonSmith <mpj17@onlinegroups.net>
2008-02-21 05:23:12 +0000

    * Wow, a case were token != value  * Now process the CSV file, and does everything *except* add or create users.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSProfile/trunk%403255

--- create_users_from_csv.py
+++ create_users_from_csv.py
@@ -11,6 +11,7 @@ from zope.schema.interfaces import ITokenizedTerm, IVocabulary,\
 from zope.interface.common.mapping import IEnumerableMapping 
 from Products.Five import BrowserView
 from Products.XWFCore.odict import ODict
+from Products.XWFCore.CSV import CSVFile
 import interfaces, utils
 
 import logging
@@ -24,6 +25,7 @@ class CreateUsersForm(BrowserView):
         self.siteInfo = createObject('groupserver.SiteInfo', context)
         self.groupsInfo = createObject('groupserver.GroupsInfo', context)
         self.profileList = ProfileList(context)
+        self.acl_users = context.site_root().acl_users
         
     @property
     def columns(self):
@@ -34,9 +36,9 @@ class CreateUsersForm(BrowserView):
             profileAttributes[pa.token] = pa.title
         
         for i in range(0, len(self.profileList)):
-            j = i + 1
-            columnId = 'column%2d' % j
-            columnTitle = u'Column %d'% j
+            j = i + 65
+            columnId = 'column%c' % chr(j)
+            columnTitle = u'Column %c'% chr(j)
             column = {
               'columnId':    columnId, 
               'columnTitle': columnTitle, 
@@ -45,7 +47,163 @@ class CreateUsersForm(BrowserView):
         assert len(retval) > 0
         return retval
         
+    def process_form(self):
+        form = self.context.REQUEST.form
+        result = {}
+        result['form'] = form
+
+        if form.has_key('submitted'):
+            result['message'] = u''
+            result['error'] = False
+            
+            r = self.process_columns(form)
+            result['message'] = '%s\n%s' % (result['message'], r['message'])
+            result['error'] = result['error'] or r['error']
+            columns = r['columns']
+
+            if not result['error']:
+                r = self.process_csv_file(form, columns)
+                result['message'] = '%s\n%s' % (result['message'], r['message'])
+                result['error'] = result['error'] or r['error']
+                csvResults = r['csvResults']
+            if not result['error']:
+                r = self.process_csv(csvResults, columns)
+                result['message'] = '%s\n%s' % (result['message'], r['message'])
+                result['error'] = result['error'] or r['error']
+
+            assert result.has_key('error')
+            assert type(result['error']) == bool
+            assert result.has_key('message')
+            assert type(result['message']) == unicode
+
+        assert type(result) == dict
+        assert result.has_key('form')
+        assert type(result['form']) == dict
+        return result
         
+    def process_columns(self, form):
+        assert type(form) == dict
+        assert 'csvfile' in form
+        
+        message = u''
+        error = False
+        
+        columns = {}
+        for key in form:
+            if 'column' in key and form[key] != 'nothing':
+                foo, col = key.split('column')
+                i = ord(col) - 65
+                columns[i] = form[key]
+                
+        requiredColumns = [p for p in self.profileList if p.value.required]
+        notSpecified = []
+
+        providedColumns = columns.values()
+        for requiredColumn in requiredColumns:
+            if requiredColumn.token not in providedColumns:
+                print requiredColumn.token 
+                notSpecified.append(requiredColumn)
+        if notSpecified:
+            error = True
+            colPlural = len(notSpecified) > 1 and 'columns have' \
+              or 'column has'
+            colM = '\n'.join(['<li>%s</li>'% c.title for c in notSpecified])
+            m = u'<p>The required %s not been specified:</p>\n<ul>%s</ul>' %\
+              (colPlural, colM)
+            message = u'%s\n%s' % (message, m)
+            
+        result = {'error':    error,
+                  'message':  message,
+                  'columns':  columns,
+                  'form':   form}
+        assert result.has_key('error')
+        assert type(result['error']) == bool
+        assert result.has_key('message')
+        assert type(result['message']) == unicode
+        assert result.has_key('columns')
+        assert type(result['columns']) == dict
+        assert len(result['columns']) >= 2
+        assert result.has_key('form')
+        assert type(result['form']) == dict
+        return result
+
+    def process_csv_file(self, form, columns):
+        message = u''
+        error = False
+        if 'csvfile' not in form:
+            m = u'<p>There was no CSV file specified. Please specify a '\
+              u'CSV file</p>'
+            message = u'%s\n%s' % (message, m)
+            error = True
+            csvfile = None
+            csvResults = None
+        else:
+            csvfile = form.get('csvfile')
+            try:
+                csvResults = CSVFile(csvfile, [str]*len(columns))
+            except AssertionError, x:
+                m = u'<p>The number of columns you have defined (%s) '\
+                  u'does not match the number of columns in the CSV file '\
+                  u'you provided.</p>' % len(columns)
+                error = True
+                message = u'%s\n%s' % (message, m)
+                csvResults = None
+        result = {'error':      error,
+                  'message':    message,
+                  'csvResults': csvResults,
+                  'form':       form}
+        assert result.has_key('error')
+        assert type(result['error']) == bool
+        assert result.has_key('message')
+        assert type(result['message']) == unicode
+        assert result.has_key('csvResults')
+        # assert isinstance(result['csvResults'], CSVFile)
+        assert result.has_key('form')
+        assert type(result['form']) == dict
+        return result
+
+    def process_csv(self, csvResults, columns):
+        assert isinstance(csvResults, CSVFile)
+        assert type(columns) == dict        
+
+        message = u'<ul>\n'
+        error = False
+        
+        # Map the data into the correctly named columns.
+        for row in csvResults.mainData:
+            fieldmap = {}
+            for column in columns:
+                fieldmap[columns[column]] = row[column]
+            r = self.process_row(fieldmap)
+            error = error or r['error']
+            message  = u'%s\n<li>%s</li>' % (message, r['message'])
+        message  = u'%s\n</ul>' % (message)
+        result = {'error':      error,
+                  'message':    message}
+        assert result.has_key('error')
+        assert type(result['error']) == bool
+        assert result.has_key('message')
+        assert type(result['message']) == unicode
+        return result
+
+    def process_row(self, fields):
+        assert type(fields) == dict
+        
+        existingUser = self.acl_users.get_userByEmail(fields['email'])
+        if existingUser:
+            fn = existingUser.getProperty('fn')
+            uid = existingUser.getId()
+            m = u'Should add user %s (%s) to the group %s (%s)' % \
+              (fn, uid, self.groupInfo.get_name(), self.groupInfo.get_id())
+        else:
+            m = u'Should create user for the address %s' % (fields['email'])
+        result = {'error':      False,
+                  'message':    m}
+        assert result.has_key('error')
+        assert type(result['error']) == bool
+        assert result.has_key('message')
+        assert type(result['message']) == unicode
+        return result
         
 class ProfileList(object):
     implements(IVocabulary, IVocabularyTokenized)
@@ -69,12 +227,12 @@ class ProfileList(object):
         
     def __iter__(self):
         """See zope.schema.interfaces.IIterableVocabulary"""
-        retval = [SimpleTerm(p, p, self.properties[p])
+        retval = [SimpleTerm(self.properties[p], p, self.properties[p].title)
                   for p in self.properties.keys()]
         for term in retval:
               assert term
               assert ITitledTokenizedTerm in providedBy(term)
-              assert term.token == term.value
+              assert term.value.title == term.title
         return iter(retval)
 
     def __len__(self):
@@ -100,7 +258,7 @@ class ProfileList(object):
         """See zope.schema.interfaces.IVocabularyTokenized"""
         for p in self.properties:
             if p == token:
-                retval = SimpleTerm(p, p, self.properties[p])
+                retval = SimpleTerm(self.properties[p], p, self.properties[p].title)
                 assert retval
                 assert ITitledTokenizedTerm in providedBy(retval)
                 assert retval.token == retval.value
@@ -113,7 +271,7 @@ class ProfileList(object):
         if len(self.__properties) == 0:
             ifs = getFieldsInOrder(self.__schema)
             for interface in ifs:
-                self.__properties[interface[0]] = interface[1].title
+                self.__properties[interface[0]] = interface[1]
         retval = self.__properties
         assert isinstance(retval, ODict)
         assert retval
