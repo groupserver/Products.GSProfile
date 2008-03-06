93d5d8e2b52e9d5fff0faff54fa70e0b83680e55
Michael JasonSmith <mpj17@onlinegroups.net>
2008-03-06 05:50:56 +0000

    Export some functions to Zope 2.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSProfile/trunk%403296

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
-import profileContextMenu, userImage, checkEmail
+import profileContextMenu, userImage, checkEmail, utils
+from AccessControl import ModuleSecurityInfo
+from AccessControl import allow_class
+utils_security = ModuleSecurityInfo('Products.GSProfile.utils')
+utils_security.declarePublic('send_verification_message')
+
 
