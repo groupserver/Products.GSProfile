0ef28e30f92ee78019c3fd4971d10cd430458585
Michael JasonSmith <mpj17@onlinegroups.net>
2008-10-06 01:38:35 +0000

    Branching, to YAPA does not crush things that work.
    --HG--
    branch : GSProfile-ABELYapa
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/branches/GSProfile-ABELYapa%404303

---
+++ userprofiles.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+from zope.publisher.interfaces import IPublishTraverse
+from zope.app.publisher.browser import queryDefaultViewName
+from zope.interface import implements
+from Products.Five import BrowserView
+
+from zope.publisher.interfaces.browser import IBrowserView
+
+from zope.publisher.interfaces.browser import IBrowserPublisher 
+
+from zope.component import getMultiAdapter, getAdapter, ComponentLookupError
+
+from Acquisition import aq_inner, aq_base, aq_chain
+
+from zExceptions import NotFound
+
+from interfaces import IGSUserProfiles
+from utils import escape_c
+
+import logging
+log = logging.getLogger('GSUserProfiles')
+
+def ec(name):
+    return ''.join([ord(d)>127 and hex(ord(d)).replace('0x', r'%') or d
+                   for d in name])
+
+class GSUserProfiles(BrowserView):
+    implements(IGSUserProfiles, IPublishTraverse)
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        
+        self.acl_users = context.acl_users
+        
+        self.traverse_subpath = []
+    
+    def publishTraverse(self, request, name):
+        retval = None
+        
+        user = (self.context.acl_users.getUser(name) or
+                self.context.acl_users.get_userByNickname(ec(name)))
+
+        if user:
+            cnn = user.get_canonicalNickname()
+            if cnn == ec(name):
+                retval = user            
+                log.info("Found user with nickname %s" % cnn)
+            else:       
+                url = '/p/%s/' % cnn
+                retval = self.request.response.redirect(url)
+        else:
+            m  = "No user with the nickname %s" % ec(name)
+            log.info(m)
+                   
+            raise NotFound, m
+   
+        assert retval != None
+        return retval
+
+
+
