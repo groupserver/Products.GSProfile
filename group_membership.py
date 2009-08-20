3dcac067c6f7d56cfb0c4d07f5cb53b5427dd40d
Michael JasonSmith <mpj17@onlinegroups.net>
2009-08-20 12:02:58 +1000

    Removing an unneeded  file.
    --HG--
    branch : GSProfile-traversal

--- group_membership.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from zope.interface import implements, alsoProvides, providedBy
-from zope.component import getUtility, createObject
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.interfaces import ITokenizedTerm, IVocabulary,\
-  IVocabularyTokenized, ITitledTokenizedTerm
-from zope.interface.common.mapping import IEnumerableMapping 
-
-
