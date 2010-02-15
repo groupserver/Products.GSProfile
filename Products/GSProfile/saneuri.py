e32b3c6231fdbe37252e60e735a36062c381224b
Michael JasonSmith <mpj17@onlinegroups.net>
2010-02-16 09:48:17 +1100

    Bug fix, and adding the "validate" method to the SaneURI schema field.

--- Products/GSProfile/saneuri.py
+++ Products/GSProfile/saneuri.py
@@ -6,8 +6,8 @@ A Sane URI Schema Field
 
 The sane URI schema field is like the standard Zope URI field, but it is
 far more tolerant of poor input. It is not unheard of for users of a
-system to enter ``groupserver.org`` as a URI, rather than 
-``http://groupserver.org``.
+system to enter domains such as ``groupserver.org`` as a URI, rather 
+than strict URIs such as <http://groupserver.org>.
 
 How dear the users fail to follow `RFC2396 
 <http://www.ietf.org/rfc/rfc2396.txt>`_ to the letter!'''
@@ -37,7 +37,7 @@ def sanitise_uri(uri):
     ------------
     
     None.'''
-    retval = url
+    retval = uri
     if not(urlparse(uri)[0]):
         retval = 'http://%s' % uri
     return retval
@@ -53,7 +53,11 @@ class SaneURI(URI):
     def constraint(self, uri):
         saneUri = sanitise_uri(uri)
         return super(SaneURI, self).constraint(saneUri)
-                
+    
+    def validate(self, uri):
+        saneUri = sanitise_uri(uri)
+        return super(SaneURI, self).validate(saneUri)
+        
     def set(self, instance, uri):
         saneUri = sanitise_uri(uri)
         return super(SaneURI, self).set(instance, saneUri)
