e9b23f3553d81b42752d76084c4324668187f004
Michael JasonSmith <mpj17@onlinegroups.net>
2010-04-08 15:55:10 +1000

    Moving the Change Password (Admin Invite) page to gs.profile.invite
    --HG--
    branch : Production

--- Products/GSProfile/interfaces.py
+++ Products/GSProfile/interfaces.py
@@ -42,19 +42,6 @@ class IGSSetPasswordRegister(IGSSetPassword):
       description=u'The page to return to after retistration has finished',
       required=False)
 
-class IGSSetPasswordAdminJoin(Interface):
-    password1 = ASCIILine(title=u'Password',
-        description=u'Your new password. For security, your password '\
-          u'should contain a mixture of letters and numbers, and '\
-          u'must be over four letters long.',
-        required=True,
-        min_length=4)
-
-    invitationId = ASCIILine(title=u'Invitation Identifier',
-      description=u'The identifier sent to you when you were invited to '\
-        u'join the group.',
-      required=True)
-
 # Address Forms
 
 class IGSEmailAddressEntry(Interface):
