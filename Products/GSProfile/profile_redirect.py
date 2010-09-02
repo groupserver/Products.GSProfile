3e44e196d6782a1944582ce0f42871faf71181bf
Michael JasonSmith <mpj17@onlinegroups.net>
2010-09-02 14:19:48 +1000

    Removing the old invitation redirectors, and using the new rsvp redirector in gs.profile.invite
    --HG--
    branch : Production

--- Products/GSProfile/profile_redirect.py
+++ Products/GSProfile/profile_redirect.py
@@ -1,15 +1,9 @@
 # coding=utf-8
-import Globals
-from Products.Five import BrowserView
-from zope.interface import implements
 from zope.component import createObject
 from Products.CustomUserFolder.interfaces import IGSUserInfo
-
 from Products.GSRedirect.view import GSRedirectBase
-
 from interfaces import *
 import utils
-
 import logging
 log = logging.getLogger('GSProfile')
 
@@ -95,100 +89,5 @@ class GSRedirectAdminVerified(GSRedirectBase):
                 uri = '/user-not-found?id=%s' % verificationId
         else: # Verification ID not specified
             uri = '/user-no-id'
-        return self.request.RESPONSE.redirect(uri)
-
-class GSRedirectRejectInvite(GSRedirectBase):
-    def __call__(self):
-        """Reject an invitation to join a group
-        """
-        site_root = self.context.site_root()
-        acl_users = site_root.acl_users
-
-        if len(self.traverse_subpath) == 1:
-            invitationId = self.traverse_subpath[0]
-            user = acl_users.get_userByInvitationId(invitationId) 
-            
-            uri = ''
-            if user:
-                userInfo = IGSUserInfo(user)
-                m = 'GSProfileRedirect: Going to reject the invitation '\
-                  'for the user %s (%s), using the invitation ID '\
-                  '"%s"' % (userInfo.name, userInfo.id, invitationId)
-                log.info(m)
-                
-                utils.login(self.context, user)
-                # Clean up
-                #user.clear_userPasswordResetVerificationIds()
-                  
-                # Get Data
-                invitation = user.get_invitation(invitationId)
-                
-                groups = getattr(site_root.Content, invitation['site_id']).groups
-                grp = getattr(groups, invitation['group_id'])
-                groupInfo = createObject('groupserver.GroupInfo', grp)
-                siteInfo = createObject('groupserver.SiteInfo', self.context)
-                
-                admin = acl_users.getUser(invitation['inviting_user_id'])
-                adrs = admin.get_preferredEmailAddresses()
-                adminAddr = ', '.join(['<%s>' % a for a in adrs])
-                
-                n_dict = {
-                  'admin': {
-                    'id':     admin.getId(),
-                    'name':   admin.getProperty('fn', ''),
-                    'email':  adminAddr,
-                  },
-                  'user': {
-                    'name':   userInfo.name,
-                    'email':  user.get_emailAddresses()[0],
-                  },
-                  'group': {
-                      'id':   groupInfo.get_id(),
-                      'name': groupInfo.get_name(),
-                      'url':  groupInfo.get_url(),
-                  },
-                  'siteName': siteInfo.get_name(),
-                  'siteURL':  siteInfo.get_url(),
-                }
-                admin.send_notification(
-                  n_type='admin_create_new_user_rejected',
-                  n_id='default',
-                  n_dict=n_dict)
-
-                if (len(user.getGroups()) <= 2):
-                    m = u'reject_invite: Deleting user "%s"' % user.getId()
-                    log.info(m)
-                    acl_users.userFolderDelUsers([user.getId()])
-                    uri = '/rejected-invitation-delete'
-                else:
-                    uri = '/rejected-invitation'
-            else: # Cannot find user
-                uri = '/user-not-found?id=%s' % invitationId
-        else: # Verification ID not specified
-            uri = '/user-no-id'
-        assert uri
-        return self.request.RESPONSE.redirect(uri)
-
-class GSRedirectAcceptInvite(GSRedirectBase):
-    def __call__(self):
-        """Accept an invitation to join a group
-        """
-        site_root = self.context.site_root()
-        acl_users = site_root.acl_users
-
-        if len(self.traverse_subpath) == 1:
-            invitationId = self.traverse_subpath[0]
-            user = acl_users.get_userByInvitationId(invitationId) 
-            if user:
-                userInfo = IGSUserInfo(user)
-                utils.login(self.context, user)
-                
-                uri = '%s/join_password.html?form.invitationId=%s' % \
-                  (userInfo.url, invitationId)
-            else: # Cannot find user
-                uri = '/user-not-found?id=%s' % invitationId
-        else: # Verification ID not specified
-            uri = '/user-no-id'
-        assert uri
         return self.request.RESPONSE.redirect(uri)
 
