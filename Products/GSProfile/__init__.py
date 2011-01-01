243f8462822805ef8691a01bb99f94ac8459ca55
Alice Murphy <alice@onlinegroups.net>
2011-01-01 19:47:42 +0100

    This util no longer exists
    --HG--
    branch : Production

--- Products/GSProfile/__init__.py
+++ Products/GSProfile/__init__.py
@@ -4,9 +4,6 @@ import profileContextMenu, userImage, utils, formwidgets, \
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
 
-utils_security = ModuleSecurityInfo('Products.GSProfile.utils')
-utils_security.declarePublic('send_add_user_notification')
-
 from utils import create_user_from_email
 utils_security = ModuleSecurityInfo('Products.GSProfile.utils')
 utils_security.declarePublic('create_user_from_email')
