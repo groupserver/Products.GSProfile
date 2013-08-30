a0f5987703b84353ddd8da247e0a96c31be7d4de
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-30 14:05:38 -0700

    Adding a copyright and license.
    --HG--
    branch : Production

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -10,15 +10,22 @@ setup(name='Products.GSProfile',
       description="",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
-        ],
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
       url='http://groupserver.org',
-      license='ZPL',
+      license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
