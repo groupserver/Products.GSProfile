1ae4964ffa332706e7179958d7da92d2ddf5da60
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-23 11:15:34 -0700

    Tidying up the old groupserver.FormWidgets content provider.
    --HG--
    branch : Production

--- Products/GSProfile/formwidgets.py
+++ Products/GSProfile/formwidgets.py
@@ -1,42 +1,38 @@
 #coding: utf-8
+from __future__ import absolute_import, unicode_literals
 from zope.pagetemplate.pagetemplatefile import PageTemplateFile
-from zope.component import adapts, provideAdapter
-import zope.interface
-import zope.publisher.interfaces
-import zope.contentprovider.interfaces 
-from Products.XWFCore import XWFUtils, ODict
-from interfaces import *
+from zope.component import adapter, provideAdapter
+from zope.interface import (implementer, Interface)
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.contentprovider.interfaces import (UpdateNotCalled,
+                                             IContentProvider)
+from .interfaces import IGSFormWidgets
 
+
+@implementer(IGSFormWidgets)
+@adapter(Interface, IDefaultBrowserLayer, Interface)
 class GSFormWidgets(object):
-    """The standard layout for GroupServer Form Widgets
-    """
-
-    zope.interface.implements(IGSFormWidgets)
-    adapts(zope.interface.Interface,
-        zope.publisher.interfaces.browser.IDefaultBrowserLayer,
-        zope.interface.Interface)
-    
+    """The standard layout for GroupServer Form Widgets"""
     def __init__(self, context, request, view):
         self.__parent__ = self.view = view
         self.__updated = False
 
         self.context = context
         self.request = request
-        
+
     def update(self):
         self.__updated = True
 
     def render(self):
         if not self.__updated:
-            raise interfaces.UpdateNotCalled
+            raise UpdateNotCalled
         pageTemplate = PageTemplateFile(self.pageTemplateFileName)
         return pageTemplate(view=self)
-        
+
     #########################################
     # Non standard methods below this point #
     #########################################
 
 provideAdapter(GSFormWidgets,
-    provides=zope.contentprovider.interfaces.IContentProvider,
-    name="groupserver.FormWidgets")
-
+               provides=IContentProvider,
+               name="groupserver.FormWidgets")
